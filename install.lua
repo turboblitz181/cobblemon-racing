c486d9ab20e286c10728c32a5971454fb2b824af
turboblitz181 <joepvandoorn@live.nl>
2026-06-18 18:08:39 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,7 +1,7 @@
 startup_Url = "https://raw.githubusercontent.com/turboblitz181/cobblemon-racing/refs/heads/main/startup.lua"
 racers_Url = "https://raw.githubusercontent.com/turboblitz181/cobblemon-racing/refs/heads/main/racers.json"
 track_1_Url = "https://raw.githubusercontent.com/turboblitz181/cobblemon-racing/refs/heads/main/tracks/track_1.json"
-
+update_Url = "https://raw.githubusercontent.com/turboblitz181/cobblemon-racing/refs/heads/main/update.lua"
 
 local request = http.get(startup_Url)
 if not request then
@@ -29,6 +29,16 @@ if not request then
     return
 end
 local file = fs.open("tracks/track_1.json", "w")
+file.write(request.readAll())
+file.close()
+request.close()
+
+local request = http.get(update_Url)
+if not request then
+    print("error getting update file")
+    return
+end
+local file = fs.open("update.lua", "w")
 file.write(request.readAll())
 file.close()
 request.close()
